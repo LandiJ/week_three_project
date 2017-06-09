ba1e3fe1931cc6b7196e2fd11feb0b39a1b277ec
Alandryia Jones <jonesalandryia@gmail.com>
2017-06-09 14:19:59 -0500

    for loop is working fine

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var button = document.querySelectorAll(".button");
-var calculator = document.querySelector("#calc_body");
+var buttons = document.querySelectorAll(".button");
+// var calculator = document.querySelector("#calc_body");
 var box = document.querySelector(".calc_box");
 var div = document.querySelector("div");
+var clear = document.querySelector("#large_c");
 
-function displayValue() {}
+console.log(buttons);
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    var value = event.target.innerHTML;
+    box.textContent += value;
+  });
+}
+
+clear.addEventListener("click", function() {
+  box.textContent = "";
+});
