447944b8663535bad53b015e6b3cee076af70d11
Alandryia Jones <jonesalandryia@gmail.com>
2017-06-09 09:32:01 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var button = document.querySelectorAll(".button");
+var calculator = document.querySelector("#calc_body");
+var box = document.querySelector(".calc_box");
+var div = document.querySelector("div");
+
+function displayValue() {}
